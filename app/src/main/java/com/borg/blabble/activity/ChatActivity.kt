e3ee70a5616123b67801368961a9394600cf5249
unxpctdchrs <theunexpectedchris@gmail.com>
2023-12-09 16:13:19 +0700

    tried to set the user pairing but failed

--- app/src/main/java/com/borg/blabble/activity/ChatActivity.kt
+++ app/src/main/java/com/borg/blabble/activity/ChatActivity.kt
@@ -2,11 +2,54 @@ package com.borg.blabble.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.borg.blabble.R
+import android.widget.Toast
+import com.borg.blabble.databinding.ActivityChatBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+
+    var firebaseuser: FirebaseUser? = null
+    var reference: DatabaseReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var intent = getIntent()
+        var userId = intent.getStringExtra("userId")
+
+        firebaseuser = FirebaseAuth.getInstance().currentUser
+        reference = FirebaseDatabase.getInstance().getReference("users").child(userId!!)
+
+        binding.btnSendMessage.setOnClickListener{
+            var message: String = binding.etMessage.text.toString()
+
+            if(message.isEmpty()){
+                Toast.makeText(applicationContext, "message is empty", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                sendMessage(firebaseuser!!.uid, userId!!, message)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun sendMessage(senderId: String, receiverId: String, message: String){
+        var reference: DatabaseReference? = FirebaseDatabase.getInstance().getReference()
+
+        var hashMap: HashMap<String, String> = HashMap()
+        hashMap.put("senderId", senderId)
+        hashMap.put("receiverId", receiverId)
+        hashMap.put("message", message)
+
+        reference!!.child("Chat").push().setValue(hashMap)
     }
 }
--- app/src/main/java/com/borg/blabble/adapter/UserAdapter.kt
+++ app/src/main/java/com/borg/blabble/adapter/UserAdapter.kt
@@ -2,6 +2,7 @@ package com.borg.blabble.adapter
 
 import android.content.Context
 import com.borg.blabble.model.Chat
+import com.borg.blabble.model.User
 
-class UserAdapter(private val context: Context, private val userList: ArrayList<Chat>) {
+class UserAdapter(private val context: Context, private val userList: ArrayList<User>) {
 }
